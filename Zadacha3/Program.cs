5186f35e5d25a3c15c9c1e9e808c27733acdb58e
Ekaterina <zvuchi89@mail.ru>
2022-09-09 08:27:48 +0300

    Zadacha3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 15:
+ Console.Write(" Введите номер дня: ");
+ int n = Convert.ToInt32(Console.ReadLine());
+ if (n > 7 || n < 1)
+ {
+    Console.WriteLine("попробуй еще ");
+ }
+  if (n == 6 || n == 7)
+ {
+    Console.WriteLine("Ехуу, выходной!"); 
+ }
+ if (n < 6 )
+ {
+    Console.WriteLine("Будни;( ");
+ } 
+
+     
+
