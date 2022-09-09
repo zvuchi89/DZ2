3db5fbfa5ab1e4580ccc220407c2f2969303982f
Ekaterina <zvuchi89@mail.ru>
2022-09-09 08:27:15 +0300

    Zadacha2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 13
+Console.Write("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n > 99)
+{
+    Console.WriteLine(n % 10);
+}
+
+ if(n < 100)
+ {
+    Console.WriteLine("третьей цифры нет");
+ }
+   
